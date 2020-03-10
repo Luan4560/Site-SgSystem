7885d532e1c704b9c4e43e9eb7c8f087a03bbe11
Luan <luan.nascimento11@outlook.com>
2020-03-10 17:32:47 -0300

    Atualização de slide no banner

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
-const items = document.querySelectorAll("img");
-const itemCount = items.length;
-const nextItem = document.querySelector(".next");
-const previousItem = document.querySelector(".previous");
-let count = 0;
+// Slide Banner
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function showNextItem() {
-  items[count].classList.remove("active");
-
-  if (count < itemCount - 1) {
-    count++;
-  } else {
-    count = 0;
-  }
-
-  items[count].classList.add("active");
-  console.log(count);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
 
-function showPreviousItem() {
-  items[count].classList.remove("active");
-
-  if (count > 0) {
-    count--;
-  } else {
-    count = itemCount - 1;
-  }
-
-  items[count].classList.add("active");
-  console.log(count);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
 }
 
-function keyPress(e) {
-  e = e || window.event;
-
-  if (e.keyCode == "37") {
-    showPreviousItem();
-  } else if (e.keyCode == "39") {
-    showNextItem();
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
   }
-}
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
-nextItem.addEventListener("click", showNextItem);
-previousItem.addEventListener("click", showPreviousItem);
-document.addEventListener("keydown", keyPress);
+  slides[slideIndex - 1].style.display = "block";
+}
